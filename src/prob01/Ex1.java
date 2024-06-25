07d2207369735098194b9c0749886dad8d94914d
kinggora <jhha09@ajou.ac.kr>
2024-06-25 12:17:36 +0900

    01. 기본배열 = 문제1, 문제2 풀이

---
+++ src/prob01/Ex1.java
@@ -0,0 +1,32 @@
+package prob01;
+
+
+public class Ex1 {
+
+  public static void main(String[] args) {
+    Ex1 ex1 = new Ex1();
+    int[] ia = new int[]{3,7,1,8,10,2,15,2,10};
+    int[] ib = new int[]{1,2,3,4,5};
+
+    ex1.sumOddEven("ia", ia);
+    ex1.sumOddEven("ib", ib);
+  }
+
+
+  public void sumOddEven(String arrName, int[] arr) {
+    int oddSum = 0;
+    int evenSum = 0;
+    for(Integer num : arr) {
+      if(num % 2 == 0) {
+        evenSum += num;
+      } else {
+        oddSum += num;
+      }
+    }
+    System.out.println("( 배열 " + arrName + " )");
+    System.out.println("홀수의 합 : " + oddSum);
+    System.out.println("짝수의 합 : " + evenSum);
+  }
+
+
+}
---
+++ src/prob01/Ex2.java
@@ -0,0 +1,19 @@
+package prob01;
+
+import java.util.Random;
+
+public class Ex2 {
+
+  public static void main(String[] args) {
+    int[] arr = new int[5];
+    Random rand = new Random();
+    // 1 ~ 100 범위의 난수를 생성하여 배열 항목 초기화
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = rand.nextInt(100) + 1; // 1 ~ 100
+    }
+    // 배열 항목 출력
+    for (Integer i : arr) {
+      System.out.print(i + " ");
+    }
+  }
+}
