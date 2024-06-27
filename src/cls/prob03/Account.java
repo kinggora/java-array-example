3f03f01843fc6be5cfcacb48452f1687edd65cfa
kinggora <jhha09@ajou.ac.kr>
2024-06-27 17:45:51 +0900

    출금 요청보다 잔고가 작으면 출금 불가 로직, 테스트 코드 추가

--- src/cls/prob03/Account.java
+++ src/cls/prob03/Account.java
@@ -1,7 +1,7 @@
 package cls.prob03;
 
 public class Account {
-  private String accNo;
+  private final String accNo;
   private int balance;
 
   public Account(String accNo) {
@@ -15,8 +15,13 @@ public class Account {
   }
 
   public void deposit(int money) {
-    this.balance -= money;
-    System.out.println(this.accNo + " 계좌에 " + money + "만원이 출금되었습니다.");
+    if(this.balance < money) {
+      System.out.println(this.accNo + " 계좌에 잔고가 부족하여 " + money + "만원 출금을 실패했습니다.");
+    } else {
+      this.balance -= money;
+      System.out.println(this.accNo + " 계좌에 " + money + "만원이 출금되었습니다.");
+
+    }
   }
 
   public String getAccNo() {
--- src/cls/prob03/TestAcc.java
+++ src/cls/prob03/TestAcc.java
@@ -19,6 +19,10 @@ public class TestAcc {
         accObj.deposit(30);
         balance = accObj.getBalance();
         System.out.println(accNo + " 계좌의 잔고는 " + balance + "만원입니다.");
+
+        accObj.deposit(100);
+        balance = accObj.getBalance();
+        System.out.println(accNo + " 계좌의 잔고는 " + balance + "만원입니다.");
     }
 
 }
