83aa3a1737864c716e426af9b3e12f11c2682910
kinggora <jhha09@ajou.ac.kr>
2024-06-28 14:26:51 +0900

    상속 03 문제 풀이

---
+++ src/inheritance/prob03/CheckingAccount.java
@@ -0,0 +1,30 @@
+package inheritance.prob03;
+
+public class CheckingAccount extends Account{
+  private String cardNo;
+
+  public CheckingAccount(String accId, String ownerName, long balance, String cardNo) {
+    super(accId, balance, ownerName);
+    this.cardNo = cardNo;
+  }
+
+  public String getCardNo() {
+    return cardNo;
+  }
+
+  public void setCardNo(String cardNo) {
+    this.cardNo = cardNo;
+  }
+
+  public void pay(long amount, String cardNo) {
+    if(cardNo.equals(this.cardNo) && getBalance() > amount) {
+      withdraw(amount);
+    } else {
+      System.out.println("지불이 불가능 합니다.");
+    }
+    System.out.println("현재잔액====>" + getBalance());
+  }
+
+  public void calcRate() {
+  }
+}
---
+++ src/inheritance/prob03/TestAccount.java
@@ -0,0 +1,10 @@
+package inheritance.prob03;
+
+public class TestAccount {
+
+  public static void main(String[] args) {
+    CheckingAccount checkingAccount = new CheckingAccount("12345", "홍길동", 1000000, "1234 5678 9012");
+    checkingAccount.pay(100000000, "1234 5678 9012");
+    checkingAccount.pay(500000, "1234 5678 9012");
+  }
+}
