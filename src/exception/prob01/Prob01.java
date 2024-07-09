66a783ebf4d0e9c1b15c09e45bdc9ff79fe25777
kinggora <jhha09@ajou.ac.kr>
2024-07-09 09:24:05 +0900

    예외처리 01, 02 문제 풀이

---
+++ src/exception/prob01/Prob01.java
@@ -0,0 +1,26 @@
+package exception.prob01;
+
+import java.util.Scanner;
+
+public class Prob01 {
+	public static void main(String[] args) {
+		Scanner scan= new Scanner(System.in);
+		System.out.println("숫자로 변환할 문자열을 입력바랍니다.");
+		String str= scan.nextLine();
+		int result = 0;
+		try {
+			result = convert(str);
+			System.out.println("변환된 숫자는 " + result + " 입니다.");
+		} catch (IllegalArgumentException e) {
+			System.out.println("예외가 발생되었습니다. 문자열을 입력하지 않고 Enter 키를 누르셨습니다.");
+		}
+	}
+	
+	private static int convert(String str) {
+		if(str == null || str.isEmpty()) {
+			throw new IllegalArgumentException();
+		}
+		return Integer.parseInt(str);
+	}
+	
+}
---
+++ src/exception/prob02/CellPhone.java
@@ -0,0 +1,37 @@
+package exception.prob02;
+
+public class CellPhone {
+  private String model;
+  private double battery;
+
+  public CellPhone(String model) {
+    this.model = model;
+  }
+
+  public void call(int time) {
+    if (time < 0) {
+      throw new IllegalArgumentException("통화시간입력오류");
+    } else if(battery - time * 0.5 < 0) {
+      battery = 0;
+    } else {
+      battery -= time * 0.5;
+    }
+    System.out.println("통화 시간 : " + time + "분");
+  }
+
+  public void charge(int time) {
+    if (time < 0) {
+      throw new IllegalArgumentException("통화시간입력오류");
+    } else if(battery + time * 3 > 100) {
+      battery = 100;
+    } else {
+      battery += time * 3;
+    }
+    System.out.println("충전 시간 : " + time + "분");
+  }
+
+  public void printBattery() {
+    System.out.println("남은 배터리 양 : " + battery);
+  }
+
+}
